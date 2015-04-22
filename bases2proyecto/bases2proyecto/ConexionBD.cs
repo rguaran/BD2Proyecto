b881de49f6a5d253b088afc2668bfc0d225bc45b
rguaran <ritaguaran@gmail.com>
2015-04-22 10:22:43 -0600

    conexion con postgresql

---
+++ bases2proyecto/bases2proyecto/ConexionBD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace bases2proyecto
+{
+    public class ConexionBD
+    {
+        String cadenaConexion = "Server=localhost;User Id=postgres;Password=admin123;Database=proyectoBD2;";
+        NpgsqlCommand miComandoSQL;
+        NpgsqlConnection miConexionBase;
+        NpgsqlDataAdapter adaptadorDatos;
+
+        public ConexionBD() { 
+        }
+
+        public void inicia()
+        {
+            miConexionBase = new NpgsqlConnection();
+            miConexionBase.ConnectionString = cadenaConexion;
+            miConexionBase.Open();
+        }
+
+        public void Crear(int id, String nombre)
+        {
+            inicia();
+            miComandoSQL = new NpgsqlCommand("insert into pruebas values (" + id + ",'" + nombre + "')", miConexionBase);
+            miComandoSQL.ExecuteNonQuery();
+            miConexionBase.Close();
+        }
+
+
+
+
+    }
+}
--- bases2proyecto/bases2proyecto/index.aspx
+++ bases2proyecto/bases2proyecto/index.aspx
@@ -5,6 +5,10 @@
     <h2>Donde aseguramos lo que más quieres</h2>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
 </asp:Content>
