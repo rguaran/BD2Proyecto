55b34ce58ef765b6f0fcd31ed084e924ad7f5895
chazter92 <cristianmucun@gmail.com>
2015-04-25 00:26:09 -0600

    insertar archivos

---
+++ bases2proyecto/bases2proyecto/archivos.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="archivos.aspx.cs" Inherits="bases2proyecto.archivos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p style="font-size: xx-large">
+        Archivos</p>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <table style="width:100%;">
+        <tr>
+            <td style="text-align: center; font-weight: 700">No. Poliza</td>
+            <td style="text-align: center">
+                <asp:TextBox ID="txtIdPoliza" runat="server"></asp:TextBox>
+            </td>
+
+        </tr>
+        <tr>
+            <td style="text-align: center; font-weight: 700">Tipo de seguro</td>
+            <td style="text-align: center">
+                <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="tipo" DataValueField="id_ts">
+                </asp:DropDownList>
+                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:proyectoBD2ConnectionString %>" ProviderName="<%$ ConnectionStrings:proyectoBD2ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM TIPO_DE_SEGURO"></asp:SqlDataSource>
+            </td>
+
+        </tr>
+
+    </table>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+
+    <p>
+        <strong>Archivo adjunto:
+        <asp:FileUpload ID="FileUpload1" runat="server" Width="245px" />
+        </strong>
+    </p>
+
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder4" runat="server">
+    <p> 
+        <asp:Button ID="btnGuardar" runat="server" Text="Guardar archivo" OnClick="btnGuardar_Click" />&nbsp;<asp:Button ID="btnConsultar" runat="server" Text="Consultar archivos" OnClick="btnConsultar_Click" />
+    </p>
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="ContentPlaceHolder5" runat="server">
+    <p>
+
+        <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click" Visible="False"></asp:LinkButton>
+        <asp:Label ID="lblFilename" runat="server" Visible="False"></asp:Label>
+
+    </p>
+</asp:Content>
---
+++ bases2proyecto/bases2proyecto/archivos.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+using System.IO;
+
+namespace bases2proyecto
+{
+    public partial class archivos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if ( FileUpload1.PostedFile != null)
+            {
+                NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=postgres;Password=admin123;Database=proyectoBD2;");
+                conn.Open();
+                FileUpload1.SaveAs(@"C:\tmp\" + FileUpload1.FileName);
+                FileStream fs = new FileStream(@"C:\tmp\" + FileUpload1.FileName, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(new BufferedStream(fs));
+                Byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                Console.WriteLine(fs.Length);
+
+                br.Close();
+                fs.Close();
+
+                NpgsqlCommand command = new NpgsqlCommand("insert into archivos(nombre,id_ts, id_poliza, archivo) values('" + FileUpload1.FileName + "','" + DropDownList1.SelectedValue + "'," + Int32.Parse(txtIdPoliza.Text) + ",:bytesData)", conn);
+                NpgsqlParameter param = new NpgsqlParameter(":bytesData", NpgsqlDbType.Bytea);
+                param.Value = bytes;
+                command.Parameters.Add(param);
+                command.ExecuteNonQuery();
+
+            }
+        }
+
+        protected void btnConsultar_Click(object sender, EventArgs e)
+        {
+            cargarAdjuntos(DropDownList1.SelectedValue, Int32.Parse(txtIdPoliza.Text));
+        }
+
+        protected void cargarAdjuntos(string id_ts, int id_poliza)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=postgres;Password=admin123;Database=proyectoBD2;");
+            conn.Open();
+            String adjunto = "";
+            ConexionBD conexion = new ConexionBD();
+            DataTable archivos = conexion.consulta("select * from archivos  where id_ts = '" + id_ts + "' and id_poliza = "+id_poliza + "and archivo is not null LIMIT 1").Tables[0];
+            foreach (DataRow archivo in archivos.Rows)
+            {
+                
+                    NpgsqlCommand command = new NpgsqlCommand("select archivo from archivos where id_archivo = " + archivo[0].ToString(), conn);
+
+                    Byte[] result = (Byte[])command.ExecuteScalar();
+                    FileStream fs = new FileStream(Server.MapPath("/archivos/" + archivo[2].ToString()), FileMode.Create, FileAccess.Write);
+                    BinaryWriter bw = new BinaryWriter(new BufferedStream(fs));
+                    bw.Write(result);
+
+                    bw.Flush();
+                    fs.Close();
+                    bw.Close();
+                    conn.Close();
+
+                    adjunto += archivo[2].ToString();
+                    lblFilename.Text = archivo[2].ToString();
+                
+            }
+
+            if (adjunto.Length > 0)
+            {
+                
+                LinkButton1.Text = adjunto;
+                LinkButton1.Visible = true;
+            }
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            if (lblFilename.Text != string.Empty)
+            {
+                if (lblFilename.Text.EndsWith(".txt"))
+                {
+                    Response.ContentType = "application/txt";
+                }
+                else if (lblFilename.Text.EndsWith(".pdf"))
+                {
+                    Response.ContentType = "application/pdf";
+                }
+                else if (lblFilename.Text.EndsWith(".docx"))
+                {
+                    Response.ContentType = "Application/msword";
+                }
+                else if (lblFilename.Text.EndsWith(".xlsx"))
+                {
+                    Response.ContentType = "Application/x-msexcel";
+                }
+                else if (lblFilename.Text.EndsWith(".jpg"))
+                {
+                    Response.ContentType = "image/jpg";
+                }
+                else if (lblFilename.Text.EndsWith(".png"))
+                {
+                    Response.ContentType = "image/png";
+                }
+                else
+                {
+                    Response.ContentType = "text/plain";
+                }
+                string filePath = "/archivos/" + lblFilename.Text;
+
+                Response.AddHeader("Content-Disposition", "attachment;filename=\"" + filePath + "\"");
+                Response.TransmitFile(Server.MapPath(filePath));
+                Response.End();
+            }
+        }
+
+
+    }
+}
